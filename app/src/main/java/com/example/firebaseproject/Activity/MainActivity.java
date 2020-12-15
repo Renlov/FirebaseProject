5ad03dcf047d1d1de5e32a4700be6a8fe0231f93
Renlov <a.pimenov95@mail.ru>
2020-12-15 20:49:21 +0300

    add main activity

--- app/src/main/java/com/example/firebaseproject/Activity/MainActivity.java
+++ app/src/main/java/com/example/firebaseproject/Activity/MainActivity.java
@@ -3,14 +3,95 @@ package com.example.firebaseproject.Activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.InputFilter;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ListView;
+import android.widget.ProgressBar;
 
-import com.example.firebaseproject.R;
+import com.example.firebaseproject.*;
+import com.example.firebaseproject.Model.Message;
+import com.example.firebaseproject.Adapter.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ListView messageListView;
+    private Adapter adapter;
+    private ProgressBar progressBar;
+    private ImageButton sendImageButton;
+    private Button sendMessageButton;
+    private EditText messageEditText;
+    private List<Message> messageList;
+
+    private String userName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        userName = "Unknown";
+
+        progressBar = findViewById(R.id.progressBar);
+        sendImageButton = findViewById(R.id.sendPhotoButton);
+        sendMessageButton = findViewById(R.id.sendMessageButton);
+        messageEditText = findViewById(R.id.massageEditText);
+        messageListView = findViewById(R.id.listView);
+
+        messageList = new ArrayList<>();
+        adapter = new Adapter(this, R.layout.message_item, messageList);
+        messageListView.setAdapter(adapter);
+
+        progressBar.setVisibility(ProgressBar.INVISIBLE);
+
+        messageEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start,
+                                          int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start,
+                                      int before, int count) {
+                //Если вводим текст, то кнопка отправки становиться видимой
+                if(s.toString().trim().length() > 0 ){
+                    sendMessageButton.setEnabled(true);
+                //Иначе кнопка будет не активной
+                } else {
+                    sendMessageButton.setEnabled(false);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+        //Ограничение по вводимому сообщению
+        messageEditText.setFilters(new InputFilter[]
+                {new InputFilter.LengthFilter(500)});
+
+        sendMessageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                messageEditText.setText("");
+            }
+        });
+
+        sendImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
     }
 }
