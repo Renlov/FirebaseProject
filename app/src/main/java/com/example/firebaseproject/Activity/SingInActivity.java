ac4d2bd8ec99f3b7ffd471c7f8d7bb51211f839c
Renlov <a.pimenov95@mail.ru>
2020-12-18 19:59:12 +0300

    try to make SingInActivity

--- app/src/main/java/com/example/firebaseproject/Activity/SingInActivity.java
+++ app/src/main/java/com/example/firebaseproject/Activity/SingInActivity.java
@@ -1,16 +1,74 @@
 package com.example.firebaseproject.Activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.firebaseproject.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class SingInActivity extends AppCompatActivity {
+
+    private FirebaseAuth auth;
+
+    private EditText emailEditText;
+    private EditText passwordEditText;
+    private EditText nameEditText;
+    private TextView toggleLoginSingUpTextView;
+    private Button loginSingUpButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sing_in);
+
+        auth = FirebaseAuth.getInstance();
+
+        emailEditText = findViewById(R.id.emailEditText);
+        passwordEditText = findViewById(R.id.passwordEditText);
+        nameEditText = findViewById(R.id.nameEditText);
+        toggleLoginSingUpTextView = findViewById(R.id.toggleLoginSingUpTextView);
+        loginSingUpButton = findViewById(R.id.loginSingUpButton);
+
+        loginSingUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loginSingUpser();
+            }
+        });
+    }
+
+    private void loginSingUpser(String email, String password) {
+
+        auth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            //Log.d(TAG, "createUserWithEmail:success");
+                            FirebaseUser user = auth.getCurrentUser();
+                            //updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            //Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(EmailPasswordActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            //updateUI(null);
+                        }
+
+                        // ...
+                    }
+                });
     }
 }
